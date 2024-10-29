908edf00ab9ce45f93ed7b6dcfe5405917860a2b
EdGilmour <159075477+EdGilmour@users.noreply.github.com>
2024-10-29 20:48:37 +0000

    Fix

--- Scripts/HelloWorld.ts
+++ Scripts/HelloWorld.ts
@@ -1,2 +1,3 @@
+// FOR TESTING: pushd index.html; python3 -m http.server 9999; popd;
 document.getElementById("Test").innerHTML = "Button Above and testing ts with div and id";
 document.body.innerHTML = "This is new text for testing";
