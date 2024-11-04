922a8ee2f065ad68f53eae4667f8b7f32190af62
EdGilmour <159075477+EdGilmour@users.noreply.github.com>
2024-11-04 20:20:37 +0000

    Pokedex Example

--- Scripts/HelloWorld.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("Test").innerHTML = "Button Above and testing ts with div and id";
-document.body.innerHTML = "This is new text for testing";
--- Scripts/HelloWorld.ts
+++
@@ -1,3 +0,0 @@
-// FOR TESTING: pushd index.html; python3 -m http.server 9999; popd;
-document.getElementById("Test").innerHTML = "Button Above and testing ts with div and id";
-document.body.innerHTML = "This is new text for testing";
